aec4e65a4a870ca576fc701b81b050db99524f35
Pablo-Cogo <80169850+Pablo-Cogo@users.noreply.github.com>
2021-03-08 01:38:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+var hamburguer = document.querySelector(".menuHamburguer");
+
+hamburguer.addEventListener("click", function(){
+	var mostrar=document.querySelector(".allMenu").classList.toggle("mostrar");
+});
